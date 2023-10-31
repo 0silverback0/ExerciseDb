e381ee0feac8ca79e894135a453791598194076e
marcell <fourmarz@yahoo.com>
2023-10-30 23:25:49 -0400

    made search views DRY

--- exercises/urls.py
+++ exercises/urls.py
@@ -1,11 +1,11 @@
 # exercises/urls.py
 from django.urls import path
-from .views import CreateExerciseView, ExerciseDetail, ExerciseList, FindExerciseByNameView, FindExerciseByPrimaryMuscleView
+from .views import CreateExerciseView, ExerciseDetail, ExerciseList, FindExerciseByFieldView
 
 urlpatterns = [
     path('create/', CreateExerciseView.as_view(), name='create-exercise'),
     path('', ExerciseList.as_view(), name='exercise-list'),
     path('<int:pk>/', ExerciseDetail.as_view(), name='exercise-detail'),
-    path('name/<str:name>', FindExerciseByNameView.as_view(), name='search'),
-    path('primary-muscle/<str:primary_muscle>', FindExerciseByPrimaryMuscleView.as_view(), name='primary-muscle')
+    path('<str:field_name>/<str:field_value>', FindExerciseByFieldView.as_view(), name='search'),
+    path('<str:field_name>/<str:field_value>', FindExerciseByFieldView.as_view(), name='primary-muscle')
 ]
--- exercises/views.py
+++ exercises/views.py
@@ -21,29 +21,27 @@ class ExerciseList(generics.ListAPIView):
     queryset = Exercise.objects.all()
     serializer_class = ExerciseSerializer
 
-class FindExerciseByNameView(APIView):
-    def get(self, request, name):
+class FindExerciseByFieldView(APIView):
+    def get(self, request, field_value, field_name):
         try:
-            exercises = Exercise.objects.filter(name__icontains=name)
+            # Define a dictionary to map field names to model fields
+            field_map = {
+                'name': 'name__icontains',
+                'primary_muscle': 'primary_muscle__icontains',
+                # Add more fields as needed
+            }
             
-            if exercises.exists():
-                serialized_exercises = ExerciseSerializer(exercises, many=True)
-                return Response(serialized_exercises.data, status=status.HTTP_200_OK)
-            else:
-                return Response({'message': 'No exercises found with that name.'}, status=status.HTTP_404_NOT_FOUND)
-        except Exercise.DoesNotExist:
-            return Response({'message': 'No exercises found with that name.'}, status=status.HTTP_404_NOT_FOUND)
-        
-class FindExerciseByPrimaryMuscleView(APIView):
-    def get(self, request, primary_muscle):
-        try:
-            exercises = Exercise.objects.filter(primary_muscle__icontains=primary_muscle)
+            field_lookup = field_map.get(field_name)
+            
+            if not field_lookup:
+                return Response({'message': 'Invalid field name.'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            exercises = Exercise.objects.filter(**{field_lookup: field_value})
 
             if exercises.exists():
                 serialized_exercises = ExerciseSerializer(exercises, many=True)
                 return Response(serialized_exercises.data, status=status.HTTP_200_OK)
             else:
-                  return Response({'message': 'No exercises found with that primary muscle.'}, status=status.HTTP_404_NOT_FOUND)
+                return Response({'message': f'No exercises found with that {field_name}.'}, status=status.HTTP_404_NOT_FOUND)
         except Exercise.DoesNotExist:
-            return Response({'message': 'No exercises found with that primary muscle.'}, status=status.HTTP_404_NOT_FOUND)
-        
+            return Response({'message': f'No exercises found with that {field_name}.'}, status=status.HTTP_404_NOT_FOUND)
