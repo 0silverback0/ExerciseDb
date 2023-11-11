b70e16a141c410315bdaf2b1ec1bfcf6f6fa0fc9
marcell <fourmarz@yahoo.com>
2023-11-11 07:08:00 -0500

    updated reqs

--- exercises/models.py
+++ exercises/models.py
@@ -7,8 +7,8 @@ class Exercise(models.Model):
     primary_muscle = models.CharField(max_length=100)
     exercise_type = models.CharField(max_length=100)
     other_names = models.TextField()  # Store a list of other names as text
-    image_url = models.URLField()
-    video_url = models.URLField()
+    image_url = models.URLField(default="https://example.com/exercise_image.jpg")
+    video_url = models.URLField(default="https://example.com/exercise_video.mp4")
     instructions = models.TextField()
     equipment = models.CharField(max_length=255)
 
--- exercises/urls.py
+++ exercises/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', ExerciseList.as_view(), name='exercise-list'),
     path('<int:pk>/', ExerciseDetail.as_view(), name='exercise-detail'),
     path('<str:field_name>/<str:field_value>', FindExerciseByFieldView.as_view(), name='search'),
-    path('<str:field_name>/<str:field_value>', FindExerciseByFieldView.as_view(), name='primary-muscle')
+    path('<str:field_name>/<str:field_value>', FindExerciseByFieldView.as_view(), name='primary-muscle'),
 ]
